581b0efb4920e47ff90edf31f0032f10f6589307
abhishek <abhishek@jcsoftwaresolution.com>
2024-09-15 23:33:28 +0530

    Add the files

---
+++ src/sept/ex_09092024/Lab009.java
@@ -0,0 +1,22 @@
+package sept.ex_09092024;
+
+public class Lab009 {
+    public static void main(String[] args) {
+        // This is single line comment
+        // This will not be executed
+
+        /*
+         * Multiple Line Comments
+         *
+         * */
+
+
+        /**
+         * Java Documentation Multiple Line Comments
+         *
+         *
+         *
+         * **/
+
+    }
+}
