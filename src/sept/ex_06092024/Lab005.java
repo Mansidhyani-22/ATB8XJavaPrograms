9acc0f974c282b42fa1929b2759b3a97a986bfd4
abhishek <abhishek@jcsoftwaresolution.com>
2024-09-15 23:33:49 +0530

    Add the files

--- src/sept/ex_06092024/Lab005.java
+++ src/sept/ex_06092024/Lab005.java
@@ -2,7 +2,16 @@ package sept.ex_06092024;
 
 public class Lab005 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args){
+        System.out.println("Hello World0!");
+        main("pramod");
     }
-}
+    public static void main(String args){
+        System.out.println("Hello World1!");
+    }
+
+    public void main(int args){
+        System.out.println("Hello World2!");
+    }
+    }
+
